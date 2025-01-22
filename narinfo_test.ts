8fb5cd3383cd32f015b789d12565d4aaa41dbc1b
Thomas Stokes <git@tombl.dev>
2025-01-22 22:28:59 +0800

    Parse nar files (#1)

--- narinfo_test.ts
+++ narinfo_test.ts
@@ -1,5 +1,6 @@
 import { assertEquals } from "@std/assert";
 import { Keychain, NIXOS_KEY } from "./keychain.ts";
+import { NarListing } from "./nar.ts";
 import { NarInfo } from "./narinfo.ts";
 
 const INFO = `
@@ -18,12 +19,17 @@ Sig: cache.nixos.org-1:TsTTb3WGTZKphvYdBHXwo6weVILmTytUjLB+vcX89fOjjRicCHmKA4RCP
 const FINGERPRINT =
   `1;/nix/store/syd87l2rxw8cbsxmxl853h0r6pdwhwjr-curl-7.82.0-bin;sha256:1b4sb93wp679q4zx9k1ignby1yna3z7c4c2ri3wphylbc2dwsys0;196040;/nix/store/0jqd0rlxzra1rs38rdxl43yh6rxchgc6-curl-7.82.0,/nix/store/6w8g7njm4mck5dmjxws0z1xnrxvl81xa-glibc-2.34-115,/nix/store/j5jxw3iy7bbz4a57fh9g2xm2gxmyal8h-zlib-1.2.12,/nix/store/yxvjs9drzsphm9pcf42a4byzj1kb9m7k-openssl-1.1.1n`;
 
+const EMPTY_NAR_LISTING: NarListing = {
+  version: 1,
+  root: { type: "directory", entries: {} },
+};
+
 const binaryCache = {
   url: new URL("https://example.com"),
   storeDir: "/nix/store",
 };
 
-const narInfo = NarInfo.parse(INFO, binaryCache);
+const narInfo = NarInfo.parse(INFO, binaryCache, EMPTY_NAR_LISTING);
 
 Deno.test("NarInfo fingerprint", () => {
   assertEquals(narInfo.fingerprint(), FINGERPRINT);
@@ -40,7 +46,11 @@ Deno.test("NarInfo signature verification", async (t) => {
       /^Sig: .*$/m,
       "Sig: cache.nixos.org-1:SGVsbG8gdGhlcmUhCg==",
     );
-    const corruptedNarInfo = NarInfo.parse(corruptedText, binaryCache);
+    const corruptedNarInfo = NarInfo.parse(
+      corruptedText,
+      binaryCache,
+      EMPTY_NAR_LISTING,
+    );
 
     assertEquals(await corruptedNarInfo.verify(keychain), {
       valid: false,
